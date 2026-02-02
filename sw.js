e89af879872398d070738adfe2f967bb249cac20
lucianodelira <138644864+lucianodelira@users.noreply.github.com>
2026-02-01 21:45:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
-self.addEventListener('install',e=>{
+self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('sala-v1').then(c=>c.addAll(['./','./index.html']))
+    caches.open('sala-v1').then(c =>
+      c.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ])
+    )
   );
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+
+self.addEventListener('activate', e => {
+  clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
